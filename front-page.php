4009a108e4d82185273dfd47999ffd65de234e9b
Antoine Ridard <papatou15@hotmail.com>
2022-05-02 10:23:43 -0400

    Ajout galerie + CSS boutons d'événements

--- front-page.php
+++ front-page.php
@@ -1,24 +1,27 @@
 <?php get_header() ?>
 <main class="site__main">
-<section class="animation">
-    <div class="animation__bloc">1</div>
-    <div class="animation__bloc">2</div>
-    <div class="animation__bloc">3</div>
-    <div class="animation__bloc">4</div>
-    <div class="animation__bloc">5</div>
-</section>
+
     <h1>Les études en TIM</h1>
     
     <?php 
     /* --------------------------------------------- menu accueil */
     wp_nav_menu(array("menu"=>"accueil",
                             "container"=>"nav")); ?>
+
+    <h1>Nos événements</h1>
     
    <?php 
    /* --------------------------------------------- menu evenement */
    wp_nav_menu(array("menu"=>"evenement",
                             "container"=>"nav",
                             "menu_class"=>"menu-evenement")); ?>
+
+    <?php if(have_posts()): the_post(); ?>
+        <h1><?php the_title() ?></h1>
+        <div class="galerie">
+            <?php the_content() ?>
+        </div>
+    <?php endif ?>
    
 </main>
 <?php get_footer() ?>
