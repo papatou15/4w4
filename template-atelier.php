af32fc3fbc90daba87fa8bfd929ca7bbf755c69d
Antoine Ridard <papatou15@hotmail.com>
2022-05-19 13:56:04 -0400

    exam-final ar - Formatage des pages d'atelier avec atelier.scss

--- template-atelier.php
+++ template-atelier.php
@@ -15,32 +15,35 @@
           <section class="atelier__description">
                <?php the_field('description'); ?>
           </section>
-          <p class="atelier__animateur">
-          Animée par: <?php the_field('animateur'); ?>
-          </p>
-          <div class="atelier__date__container">
-            <p class="atelier__date__debut">
-            Du: <?php the_field('date_de_debut_de_latelier'); ?>
+          <div class="atelier__info">
+            <p class="atelier__animateur">
+            <b>Animée par: </b><i><?php the_field('animateur'); ?></i>
             </p>
-            <p class="atelier__date__fin">
-            Au: <?php the_field('date_de_fin_de_latelier'); ?>
+            <div class="atelier__date__container">
+                <p class="atelier__date__debut">
+                <b> Du: </b> <i><?php the_field('date_de_debut_de_latelier'); ?></i>
+                </p>
+                <p class="atelier__date__fin">
+                <b> Au: </b><i><?php the_field('date_de_fin_de_latelier'); ?></i>
+                </p>
+            </div>
+            <div class="atelier__heure__container">
+                <p class="atelier__heure__debut">
+                <b> De: </b><i><?php the_field('heure_de_debut'); ?></i>
+                </p>
+                <p class="atelier__heure__fin">
+                <b>Jusqu'à: </b><i><?php the_field('heure_de_fin'); ?></i>
+                </p>
+            </div>
+            
+            <p class="atelier__duree__seance">
+            <b> Durée d'une séance: </b><i><?php the_field('duree_dune-séance-datelier'); ?>H</i>
             </p>
-          </div>
-          <div class="atelier__heure__container">
-            <p class="atelier__heure__debut">
-            De: <?php the_field('heure_de_debut'); ?>
-            </p>
-            <p class="atelier__heure__fin">
-            Jusqu'à: <?php the_field('heure_de_fin'); ?>
+            <p class="atelier__local">
+            <b>Au local</b> <i><?php the_field('local'); ?></i>
             </p>
           </div>
           
-          <p class="atelier__duree__seance">
-          Durée d'une séance: <?php the_field('duree_dune-seance-datelier'); ?>H
-          </p>
-          <p class="atelier__local">
-          Au local <?php the_field('local'); ?>
-          </p>
      </article>
      <?php endif ?>
 </main>
